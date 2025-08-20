576891bbbe80ebc2a6083931c38e34058ba73f98
rogawa14106 <ookawa14106@gmail.com>
2025-08-21 00:22:22 +0900

    feat: assetsを追加

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755701935',resources: [
+    cacheName: 'chirpy-1755703332',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
